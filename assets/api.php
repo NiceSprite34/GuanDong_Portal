c6496a9b73349643cd572649e76aadceae19642d
NiceSprite <1624399331@qq.com>
2021-03-09 15:56:45 +0800

    Add files via upload

---
+++ assets/api.php
@@ -0,0 +1,71 @@
+<?php
+#Created by Myitmx on 2019/10/16.
+#Copyright © 2019年 Myitmx. All rights reserved.
+#广东天翼校园网第三方登录 PHP版
+
+namespace Myitmx;
+
+class xyw
+{
+	public function post_json($url, $jsonStr)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonStr);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json; charset=utf-8'
+		)
+		);
+		$response = curl_exec($ch);
+		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+		return array($httpCode, $response);
+	}
+	
+	public function getVerifyCode($username,$wlanuserip,$wlanacip,$mac)
+	{
+		$secret = "Eshore!@#";
+		$time = time();
+		$authenticator = $wlanuserip.$wlanacip.$mac.$time.$secret;
+		$authenticator = strtoupper(md5($authenticator));
+		$post_date = array('username' => $username, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator, 'iswifi' => "4060");
+		$post_date = json_encode($post_date);
+		$VerifyCode = xyw::post_json("http://61.140.12.23:10001/client/challenge", $post_date);
+		$VerifyCode = json_decode($VerifyCode['1'],true);
+		if($VerifyCode['rescode'] == "0")
+		{
+			return $VerifyCode['challenge'];
+
+		}
+		else
+		{
+			return "获取验证码出错！！！";
+		}
+	}
+	
+	public function Login($username,$password,$wlanuserip,$wlanacip,$mac,$verificationcode)
+	{
+		$secret = "Eshore!@#";
+		$time = time();
+		$authenticator = $wlanuserip.$wlanacip.$mac.$time.$verificationcode.$secret;
+		$authenticator = strtoupper(md5($authenticator));
+		$post_date = array('username' => $username, 'password' => $password, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator, 'iswifi' => "4060", 'verificationcode'=>$verificationcode);
+		$post_date = json_encode($post_date);
+		$Login_return = xyw::post_json("http://61.140.12.23:10001/client/login", $post_date);
+		return $Login_return['1'];
+	}
+
+	public function Logout($username,$wlanuserip,$wlanacip,$mac)
+	{
+		$secret = "Eshore!@#";
+		$time = time();
+		$authenticator = $wlanuserip.$wlanacip.$mac.$time.$secret;
+		$authenticator = strtoupper(md5($authenticator));
+		$post_date = array('username' => $username, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator);
+		$post_date = json_encode($post_date);
+		$Logout_return = xyw::post_json("http://125.88.59.131:10001/client/logout", $post_date);
+		return $Logout_return['1'];
+	}
+}
---
+++ header.php
@@ -0,0 +1,40 @@
+<html lang="zh-cn">
+<!-- https://github.com/NiceSprite34/ --> 
+<!-- Github@NiceSprite34 --> 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="Cache-Control" content="no-siteapp">
+    <title>飞Young - Login</title>
+    <link rel="stylesheet" href="./assets/css/mdui.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script type="text/javascript" src="./assets/js/mdui.min.js"></script>
+ </head>
+<!-- 导航开始 --> 
+<body class="mdui-drawer-body-left mdui-appbar-with-toolbar mdui-bottom-nav-fixed ">
+    <div class="background"><!--背景 --></div>    
+    <div class="mdui-appbar-with-toolbar">
+        <div class="mdui-appbar mdui-appbar-fixed mdui-headroom">
+            <div class="mdui-toolbar">
+                <button mdui-drawer="{target: '.mdui-drawer', overlay: true}" class="mdui-btn mdui-btn-icon">
+                    <i class="mdui-icon material-icons">menu</i>
+                </button>
+                <a class="mdui-typo-headline" href="/">飞Young - Login</a>
+                <div class="mdui-typo-title mdui-hidden-xs-down">首页</div>
+                <div class="mdui-toolbar-spacer"></div>
+            
+            </div>
+        </div>
+        <!-- 抽柜开始 --> 
+        <div class="mdui-drawer mdui-drawer-close mdui-color-white mdui-drawer-full-height">
+            <div class="mdui-list">
+                <a class="mdui-list-item mdui-ripple mdui-text-color-theme mdui-list-item-active" href="/">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+                    <div class="mdui-list-item-content">首页</div>
+                </a>
+            </div>
+            <div class="copyright"></div>
+        </div>
+    </div>

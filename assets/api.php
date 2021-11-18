6d347f6868e18432614686514cc668a9673eae74
NiceSprite <1624399331@qq.com>
2021-11-18 09:36:24 +0800

    Update api.php

--- assets/api.php
+++ assets/api.php
@@ -1,10 +1,9 @@
 <?php
-#Created by Myitmx on 2019/10/16.
-#Copyright © 2019年 Myitmx. All rights reserved.
-#广东天翼校园网第三方登录 PHP版
+#Update by NiceSprite 2021-11-15
+#Copyright © 2019 Myitmx. All rights reserved.
+#广东天翼校园网第三方登录 重构版
 
 namespace Myitmx;
-
 class xyw
 {
 	public function post_json($url, $jsonStr)
@@ -28,11 +27,13 @@ class xyw
 	{
 		$secret = "Eshore!@#";
 		$time = time();
-		$authenticator = $wlanuserip.$wlanacip.$mac.$time.$secret;
+		$version = "214";
+		    
+		$authenticator = $version.$wlanuserip.$wlanacip.$mac.$time.$secret;
 		$authenticator = strtoupper(md5($authenticator));
-		$post_date = array('username' => $username, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator, 'iswifi' => "4060");
+		$post_date = array('version' => $version, 'username' => $username, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator, 'iswifi' => "4060");
 		$post_date = json_encode($post_date);
-		$VerifyCode = xyw::post_json("http://61.140.12.23:10001/client/challenge", $post_date);
+		$VerifyCode = xyw::post_json("http://enet.10000.gd.cn:10001/client/vchallenge", $post_date);
 		$VerifyCode = json_decode($VerifyCode['1'],true);
 		if($VerifyCode['rescode'] == "0")
 		{
@@ -44,28 +45,37 @@ class xyw
 			return "获取验证码出错！！！";
 		}
 	}
-	
-	public function Login($username,$password,$wlanuserip,$wlanacip,$mac,$verificationcode)
+
+public function Login($username,$password,$wlanuserip,$wlanacip,$mac,$verificationcode)
 	{
 		$secret = "Eshore!@#";
 		$time = time();
 		$authenticator = $wlanuserip.$wlanacip.$mac.$time.$verificationcode.$secret;
 		$authenticator = strtoupper(md5($authenticator));
-		$post_date = array('username' => $username, 'password' => $password, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator, 'iswifi' => "4060", 'verificationcode'=>$verificationcode);
+		$post_date = array('username' => $username, 'password' => $password, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator, 'iswifi' => "1050", 'verificationcode'=>$verificationcode);
 		$post_date = json_encode($post_date);
 		$Login_return = xyw::post_json("http://61.140.12.23:10001/client/login", $post_date);
-		return $Login_return['1'];
-	}
+		return $Login_return['1'];	
+			}
 
-	public function Logout($username,$wlanuserip,$wlanacip,$mac)
+
+public function Keep($username,$wlanuserip,$wlanacip,$mac)
 	{
 		$secret = "Eshore!@#";
 		$time = time();
-		$authenticator = $wlanuserip.$wlanacip.$mac.$time.$secret;
-		$authenticator = strtoupper(md5($authenticator));
-		$post_date = array('username' => $username, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $authenticator);
-		$post_date = json_encode($post_date);
-		$Logout_return = xyw::post_json("http://125.88.59.131:10001/client/logout", $post_date);
-		return $Logout_return['1'];
-	}
+		$kee = $wlanuserip.$wlanacip.$mac.$time.$secret;
+		$keep = strtoupper(md5($kee));		
+	    $post_da = array('username' => $username, 'clientip' => $wlanuserip, 'nasip' => $wlanacip, 'mac' => $mac, 'timestamp' => $time, 'authenticator' => $keep);
+	    $get_keep = "username=$username&clientip=$wlanuserip&nasip=$wlanacip&mac=$mac&timestamp=$time&authenticator=$keep";			
+		$urls="http://enet.10000.gd.cn:8001/hbservice/client/active?$get_keep";
+        $keeps = file_get_contents($urls);   
+        echo $keeps;	
+        
+        
+        
+        
+        
+			}
+
+
 }
